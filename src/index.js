079d5d1f8a12ff11d9fac4b76f122641a01e2166
limichange <limichange@hotmail.com>
2017-01-19 12:45:01 +0800

    🚨  - Removing linter warnings.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ Object.keys = Object.keys || require('object-keys')
  Doc: http://developer.qiniu.com/code/v6/api/kodo-api/image/imageview2.html
 */
 
-module.exports = (url = '', options, type = 'imageView2') => {
+module.exports = function (url = '', options, type = 'imageView2') {
   if (options && isString(url)) {
     url = `${url}?${type}`
 
